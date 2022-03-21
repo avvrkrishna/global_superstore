5dfc2a34daa31232c6d5de3a8c9d7db8f76018b9
avvrkrishna <vv.adabala@gmail.com>
2022-03-21 14:24:54 +0530

    Updated incremental strategy for customer dim

--- models/master_data/gold/global_product_dim.sql
+++ models/master_data/gold/global_product_dim.sql
@@ -10,9 +10,5 @@ select
     product_name,
     category as product_category,
     sub_category as product_sub_category,
-    record_created_datetime
-from {{ref('global_superstore_sales_fact')}}
-{% if is_incremental() %}
-where record_created_datetime >
-(select max(record_created_datetime) from {{this}})
-{% endif %}
+    current_timestamp() as record_created_datetime
+from {{ref('stage_superstore_orders')}}
